520d04397d21378dc48f31292cb2a3aafa1ee19f
lisa-valentina <lisa.riedel@gmail.com>
2025-02-19 20:45:42 +0100

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import WeatherApp from "./WeatherApp";
-import Footer from "./Footer";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
