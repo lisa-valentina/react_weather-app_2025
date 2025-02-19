f4ed603ffeb9f0273582deaae061134b0c967d97
lisa-valentina <lisa.riedel@gmail.com>
2025-02-19 20:38:26 +0100

    Created and added footer

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        This weather app was coded by Lisa Valentina Riedel with SheCodes. It is
+        open-source on{" "}
+        <a href="https://github.com/lisa-valentina/react_weather-app_2025">
+          github
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
---
+++ src/WeatherApp.js
@@ -0,0 +1,11 @@
+import "./WeatherApp.css";
+import Footer from "./Footer";
+
+export default function WeatherApp() {
+  return (
+    <div className="WeatherApp">
+      Weather app
+      <Footer />
+    </div>
+  );
+}
