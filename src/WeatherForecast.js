150c4787a16b9b60fb28d85d641e1f05f4f8359b
lisa-valentina <lisa.riedel@gmail.com>
2025-02-21 17:50:46 +0100

    added forecast structure

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,8 +3,65 @@ import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
   return (
-    <div className="WeatherForecast">
-      Here, you will see the weather forecast.
+    <div className="container">
+      <div className="row weather">
+        <div className="col">
+          Sunday
+          <br />
+          Mostly cloudy <br />
+          12° 8°
+          <br />
+          Monday
+          <br />
+          Mostly cloudy
+          <br />
+          16° 5°
+          <br />
+          Tuesday
+          <br />
+          Cloudy
+          <br />
+          17° 11°
+        </div>
+
+        <div className="col-1 icons-left">
+          <br />
+          <i className="fa-solid fa-cloud-sun weather-icons"></i>
+          <br />
+          <br />
+          <i className="fa-solid fa-cloud-sun weather-icons"></i>
+          <br />
+          <br />
+          <i className="fa-solid fa-cloud weather-icons"></i> <br />
+        </div>
+        <div className="col-1 weather-border icons-right">
+          <br />
+          <i className="fa-solid fa-cloud-rain weather-icons"></i>
+          <br />
+          <br />
+          <i className="fa-solid fa-cloud-sun weather-icons"></i>
+          <br />
+          <br />
+          <i className="fa-solid fa-cloud-sun weather-icons"></i> <br />
+        </div>
+        <div className="col weather-forecast">
+          Wednesday
+          <br />
+          Showers
+          <br />
+          14° 8° <br />
+          Thursday
+          <br />
+          Partly cloudy <br />
+          14° 6°
+          <br />
+          Friday
+          <br />
+          Partly cloudy
+          <br />
+          13° 5°
+        </div>
+      </div>
     </div>
   );
 }
